ffbe1bcd556115f2d204f7031d4c9e43247a71cd
Yohan Boniface <yb@enix.org>
2013-01-07 19:06:30 +0100

    Fix for pypi upload

--- setup.py
+++ setup.py
@@ -9,8 +9,14 @@ import leaflet_storage
 
 long_description = codecs.open('README.rst', "r", "utf-8").read()
 
+with open('requirements.pip') as reqs:
+    install_requires = [
+        line for line in reqs.read().split('\n') if (line and not
+                                                     line.startswith(('--', 'git')))
+    ]
+
 setup(
-    name="leaflet_storage",
+    name="django-leaflet-storage",
     version=leaflet_storage.__version__,
     author=leaflet_storage.__author__,
     author_email=leaflet_storage.__contact__,
@@ -22,12 +28,12 @@ setup(
     include_package_data=True,
     platforms=["any"],
     zip_safe=True,
-    install_requires=["vectorformats", ],
+    install_requires=install_requires,
     long_description=long_description,
 
     classifiers=[
         "Development Status :: 3 - Alpha",
-        #"Environment :: Web Environment",
+        "Environment :: Web Environment",
         "Intended Audience :: Developers",
         #"License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
