8291360eb8854fe8ac6b5fa3f7f8c7aff317e6c7
Yohan Boniface <yb@enix.org>
2013-06-16 13:17:32 +0200

    Make possible to edit TileLayer ranks from admin list view

--- leaflet_storage/admin.py
+++ leaflet_storage/admin.py
@@ -3,10 +3,14 @@ from .models import Map, Marker, DataLayer, Pictogram, TileLayer, Polyline,\
                     Licence
 
 
+class TileLayerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'rank', )
+    list_editable =('rank', )
+
 admin.site.register(Map, admin.OSMGeoAdmin)
 admin.site.register(Marker, admin.OSMGeoAdmin)
 admin.site.register(Polyline, admin.OSMGeoAdmin)
 admin.site.register(DataLayer)
 admin.site.register(Pictogram)
-admin.site.register(TileLayer)
+admin.site.register(TileLayer, TileLayerAdmin)
 admin.site.register(Licence)
