c632b705f2cd2399a8ef6bb3c8aceda5f1ee9436
Yohan Boniface <yb@enix.org>
2014-06-26 21:13:39 +0200

    Use locale form of language code in sotragei18n command
    This is what is used by Transifex, so by Leaflet.Storage

--- leaflet_storage/management/commands/storagei18n.py
+++ leaflet_storage/management/commands/storagei18n.py
@@ -5,16 +5,18 @@ from django.core.management.base import BaseCommand
 from django.conf import settings
 from django.contrib.staticfiles import finders
 from django.template.loader import render_to_string
+from django.utils.translation import to_locale
 
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
         for code, name in settings.LANGUAGES:
+            code = to_locale(code)
             print "Processing", name
             path = finders.find('storage/src/locale/{code}.json'.format(code=code))
             if not path:
-                print "No file at", path, "Skipping"
+                print "No file for", code, "Skipping"
             else:
                 with io.open(path, "r", encoding="utf-8") as f:
                     print "Found file", path
